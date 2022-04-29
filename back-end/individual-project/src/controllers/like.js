24336e81e184b65fcfbc284f1f1a993bd41efcd8
wahyuoctar <wahyuoctar@gmail.com>
2022-04-29 13:11:43 +0700

    feat: like and unlike backend

--- back-end/individual-project/src/controllers/like.js
+++ back-end/individual-project/src/controllers/like.js
@@ -32,7 +32,7 @@ const likeControllers = {
         }
     },
 
-    likeUnlikePost: async(req, res) => {
+    likePost: async(req, res) => {
         try {
             const {postId} = req.params
             const {user_id} = req.body
@@ -50,31 +50,16 @@ const likeControllers = {
                 })
             }
 
-            const findUserLike = await Like.findAll({
+            const findUserLike = await Like.findOne({
                 where: {
                     post_id: postId,
                     user_id
                 }
             })
 
-            if (!findUserLike) {
-                await Post.decrement({
-                    like_count: 1
-                }, {
-                    where: {
-                        id: postId,
-                    }
-                })
-
-                await Like.destroy({
-                    where: {
-                        post_id: postId,
-                        user_id
-                    }
-                })
-
-                return res.status(200).json({
-                    message: "You already Unlike this Post!"
+            if (findUserLike) {
+                return res.status(400).json({
+                    message: "You Already Like This Post!"
                 })
             }
 
@@ -97,6 +82,62 @@ const likeControllers = {
                 result: likePost
             })
 
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json({
+                message: "Can't Reach Server"
+            })
+        }
+    },
+
+    unlikePost: async(req, res) => {
+        try {
+            const {postId} = req.params
+            const {user_id} = req.body
+            // const user_id = req.token.user_id
+
+            const findPost = await Post.findAll({
+                where: {
+                    id: postId
+                }
+            })
+
+            const findUserLike = await Like.findOne({
+                where: {
+                    post_id: postId,
+                    user_id
+                }
+            })
+
+            if (!findPost) {
+                return res.status(400).json({
+                    message: "Can't find this Post!"
+                })
+            }
+
+            if (!findUserLike) {
+            return res.status(400).json({
+                message: "You Already Unlike This Post!"
+            })}
+
+            await Post.decrement({
+                like_count: 1
+            }, {
+                where: {
+                    id: postId,
+                }
+            })
+
+            await Like.destroy({
+                where: {
+                    post_id: postId,
+                    user_id
+                }
+            })
+
+            return res.status(200).json({
+                message: "Unlike Post Successfully!"
+            })
         } catch (err) {
             console.log(err);
             return res.status(500).json({
--- back-end/individual-project/src/routes/like.js
+++ back-end/individual-project/src/routes/like.js
@@ -3,6 +3,7 @@ const likeControllers = require("../controllers/like")
 const router = require("express").Router()
 
 router.get("/post/:postId", likeControllers.getPostLike)
-router.post("/post/:postId", likeControllers.likeUnlikePost)
+router.post("/post/:postId", likeControllers.likePost)
+router.delete("/post/:postId", likeControllers.unlikePost)
 
 module.exports = router
